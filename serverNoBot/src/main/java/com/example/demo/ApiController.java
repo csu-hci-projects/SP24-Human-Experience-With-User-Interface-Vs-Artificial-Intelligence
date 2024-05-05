c0bb07df62ccedb26d1772224a1bb1b0f5ac841c
bbgabel <bbgabel5@gmail.com>
2024-05-04 18:15:27 -0600

    Server no bot

---
+++ serverNoBot/src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class ApiController {
+
+    @PostMapping("/api")
+    public ResponseEntity<String> handlePostRequest(@RequestBody MyRequestData requestData) {
+
+        Caller caller = new Caller();
+        Calculations calculation = new Calculations(requestData.age, requestData.height, requestData.weight, requestData.desiredWeight, requestData.sex, requestData.activity, requestData.body);
+        String data = caller.useData(calculation);
+
+        return ResponseEntity.ok(data);
+    }
+    
+}
---
+++ serverNoBot/src/main/java/com/example/demo/MyRequestData.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class MyRequestData {
+    public int age;
+    public String height;
+    public int weight;
+    public int desiredWeight;
+    public String sex;
+    public String activity;
+    public int body;
+}
